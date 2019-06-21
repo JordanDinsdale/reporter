b8329d0d8f0f986f5a0ba3ab1bfc535796266879
Jordan <jordan@admins-MacBook-Air.local>
2019-06-21 09:11:03 +0100

    Changed Title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Reporter
                 </div>
 
                 <div class="links">
